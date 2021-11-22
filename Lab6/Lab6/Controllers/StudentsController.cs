8cbd397a48ac40a3f7270a710313f0ae7df356b5
Prince Felix <princeafelix@gmail.com>
2021-11-22 14:59:56 -0500

    GetByID, Create and Deete Actions Complete

---
+++ Lab6/Lab6/Controllers/StudentsController.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.AspNetCore.Mvc;
+   
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Lab6.Data;
+using Lab6.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace Lab6.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentsController : ControllerBase
+    {
+
+        private readonly StudentDbContext _context;
+
+        public StudentsController(StudentDbContext context)
+        {
+            _context = context;
+        }
+
+
+        /// <summary>
+        /// Get collection of Students.
+        /// </summary>
+        /// <returns>A collection of Students</returns>
+        /// <response code="200">Returns a collection of Students</response>
+        /// <response code="500">Internal error</response>      
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<IEnumerable<Student>>> Get()
+        {
+            return Ok(await _context.Students.ToListAsync());
+        }
+
+        // GET: Cars/d2cab0c9-7e94-409e-2b9a-08d88428ae4a
+        /// <summary>
+        /// Get a Car.
+        /// </summary>
+        /// <param id="id"></param>
+        /// <returns>A Car</returns>
+        /// <response code="201">Returns a collection of Cars</response>
+        /// <response code="400">If the id is malformed</response>      
+        /// <response code="404">If the Car is null</response>      
+        /// <response code="500">Internal error</response>
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<Student>> GetById(Guid id)
+        {
+            Student student = await _context.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return Ok(student);
+        }
+
+        // POST: Cars
+        /// <summary>
+        /// Creates a Car.
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     POST /Cars
+        ///     {
+        ///        "make": "make",
+        ///        "price": 0
+        ///     }
+        ///
+        /// </remarks>
+        /// <returns>A newly created Car</returns>
+        /// <response code="201">Returns the newly created Car</response>
+        /// <response code="400">If the Car is malformed</response>      
+        /// <response code="500">Internal error</response>
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<Student>> CreateAsync([Bind("FirstName,LastName,Programm")] StudentBase studentBase)
+        {
+            Student student = new Student
+            {
+                FirstName = studentBase.FirstName,
+                LastName = studentBase.LastName,
+                Program = studentBase.Program
+
+            };
+
+            _context.Add(student);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetById), new { id = student.ID }, student);
+        }
+
+
+        // DELETE: Cars/5
+        /// <summary>
+        /// Deletes a Car.
+        /// </summary>
+        /// <param id="id"></param>
+        /// <response code="202">Car is deleted</response>
+        /// <response code="400">If the id is malformed</response>      
+        /// <response code="500">Internal error</response>
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status202Accepted)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var student = await _context.Students.FindAsync(id);
+            _context.Students.Remove(student);
+            await _context.SaveChangesAsync();
+            return Accepted();
+        }
+
+
+
+    }
+}
