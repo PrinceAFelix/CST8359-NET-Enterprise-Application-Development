21ca97ec64aadfa8810b58cb3cb64bdd9d058116
Prince Felix <princeafelix@gmail.com>
2021-12-08 12:16:50 -0500

    Finished Assignment 2

---
+++ Assignment2/Models/Advertisement.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment2.Models
+{
+    public class Advertisement
+    {
+        [Required]
+        public int id
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public string CommunityId
+        { 
+            get; 
+            set;
+        }
+
+        public Community Community 
+        { 
+            get; 
+            set; 
+        }
+
+        [Required]
+        [DisplayName("File Name")]
+        public string FileName
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [Url]
+        [DisplayName("Image")]
+        public string Url
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ Assignment2/Views/Students/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Assignment2.Data;
+using Assignment2.Models;
+
+namespace Assignment2.Views.Students
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Assignment2.Data.SchoolCommunityContext _context;
+
+        public CreateModel(Assignment2.Data.SchoolCommunityContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Student Student { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Students.Add(Student);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
